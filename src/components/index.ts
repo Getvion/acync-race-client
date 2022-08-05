3a5a5647d7505822584de726b851d47d6c202cce
Oleg Somov <gog0125@mail.ru>
2022-08-05 17:49:41 +0300

    feat: add table generation for winners page

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,4 @@ export { Race } from './race/race';
 export { CarTrack } from './carTrack/carTrack';
 export { API } from './api/api';
 export { App } from './app/app';
+export { Winners } from './winners/winners';
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
-import { App, Generate, CarTrack, Control, Race, API } from './components/';
-import { ICar } from './types';
+import { App, Generate, CarTrack, Control, Race, API, Winners } from './components/';
 
 const app = new App();
 const generate = new Generate();
@@ -7,9 +6,38 @@ const control = new Control();
 const race = new Race();
 const carTrack = new CarTrack();
 const api = new API();
+const winners = new Winners();
 
-generate.generateApp();
-generate.generateGarage(control, race, carTrack, api, app);
-generate.generateGarageListeners(carTrack, api, app);
-race.generateRaceListeners(carTrack, api, app);
-carTrack.createTrack(api.getCars(app.garagePage));
+const navLayout = `<nav class="nav">
+<button class="nav__button nav__button-garage">garage</button>
+<button class="nav__button nav__button-winners">winners</button>
+</nav>
+<div class='app'></div>`;
+
+const body = document.querySelector('body') as HTMLElement;
+body.innerHTML = navLayout;
+
+const garageBtn = document.querySelector('.nav__button-garage') as HTMLButtonElement;
+const winnerBtn = document.querySelector('.nav__button-winners') as HTMLButtonElement;
+
+garageBtn?.addEventListener('click', () => {
+  const appElement = document.querySelector('.app') as HTMLElement;
+  appElement.innerHTML = '';
+  generate.generateGarage(control, race, carTrack, api, app);
+  generate.generateGarageListeners(carTrack, api, app);
+  race.generateRaceListeners(carTrack, api, app);
+  carTrack.createTrack(api.getCars(app.garagePage));
+  garageBtn.disabled = true;
+  winnerBtn.disabled = false;
+});
+garageBtn.click();
+
+winnerBtn.addEventListener('click', () => {
+  garageBtn.disabled = false;
+  winnerBtn.disabled = true;
+
+  const appElement = document.querySelector('.app') as HTMLElement;
+  appElement.innerHTML = '';
+
+  generate.generateWinners(winners, api);
+});
