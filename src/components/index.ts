8a41ef461996020b0de92b3818e13f89be25ecd3
Oleg Somov <gog0125@mail.ru>
2022-08-08 21:25:46 +0300

    feat: deploy

--- src/components/index.ts
+++
@@ -1,7 +0,0 @@
-export { Generate } from './generate/generate';
-export { Control } from './controll/controll';
-export { Race } from './race/race';
-export { CarTrack } from './carTrack/carTrack';
-export { API } from './api/api';
-export { App } from './app/app';
-export { Winners } from './winners/winners';
